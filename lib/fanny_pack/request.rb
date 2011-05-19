dadf04d60cf46306bbc67b6820ebf29936424e3d
Joshua Priddle <jpriddle@nevercraft.net>
2011-05-19 13:55:29 -0400

    Refactor

--- lib/fanny_pack/request.rb
+++ lib/fanny_pack/request.rb
@@ -4,43 +4,25 @@ module FannyPack
   class Request
     attr_reader :params, :response
 
-    WSDL = "https://netenberg.com/api/"
+    VALID_ACTIONS = [
+      :getIpList, :getIpListDetailed, :getIpDetails, :addIp,
+      :editIp, :deactivateIp, :reactivateIp, :deleteIp
+    ].freeze
 
-    def self.run(cmd, params = {})
-      command = case cmd.to_sym
-      when :add
-        if params[:type]
-          params[:type] = FannyPack::IP::IP_TYPES[params[:type]]
-        end
-        :add_ip
-      when :edit
-        :edit_ip
-      when :delete
-        :delete_ip
-      when :reactivate
-        :reactivate_ip
-      when :deactivate
-        :deactivate_ip
-      when :list
-        if params[:details]
-          params.delete :details
-          :get_ip_list_detailed
-        else
-          :get_ip_list
-        end
-      when :details
-        :get_ip_details
-      else
-        raise ArgumentError, "Invalid action #{cmd}"
-      end
-      params[:accountHASH] = FannyPack.account_hash
-      self.new.commit params
+    URL = "https://netenberg.com/api/"
+
+    def initialize
+      @action   = :invalid
+      @response = {}
+      @params   = {}
     end
 
     def commit(action, params = {})
+      unless VALID_ACTIONS.include? action.to_sym
+        raise "Invalid action"
+      end
       @action = action
       @params = params
-      @response = {}
     end
 
     def to_xml
--- spec/fanny_pack/request_spec.rb
+++ spec/fanny_pack/request_spec.rb
@@ -1,21 +1,33 @@
 require 'spec_helper'
 
 describe FannyPack::Request do
+
+  describe "::VALID_ACTIONS" do
+    it { FannyPack::Request::VALID_ACTIONS.should be_frozen }
+  end
+
   before :each do
     @req = FannyPack::Request.new
   end
 
-  describe "::run" do
-    it "raises ArgumentError on invalid command" do
-      expect {
-        FannyPack::Request.run :bad_command
-      }.to raise_error(ArgumentError)
+  describe "::new" do
+    %w[response params].each do |test|
+      it "sets @#{test} to a hash" do
+        @req.instance_variable_get("@#{test}").should respond_to :keys
+      end
+    end
+  end
+
+  %w[response params].each do |test|
+    describe "##{test}" do
+      it "returns @#{test}" do
+        @req.instance_variable_get("@#{test}").should == @req.send(test)
+      end
     end
   end
 
   describe "#to_xml" do
     it "builds a SOAP envelope" do
-      @req.commit :list, :type => '1'
       xml = @req.to_xml
       xml.should include
         %{<env:Envelope xmlns:env="http://schemas.xmlsoap.org/soap/envelope/">\n  <env:Body>}
@@ -26,6 +38,18 @@ describe FannyPack::Request do
   end
 
   describe "#commit" do
+    before :each do
+      @req.commit :getIpList, :type => 1
+    end
+
+    it "raises exception unless action is valid" do
+      expect { @req.commit :HammerTime }.to raise_error
+    end
+
+    it "sets the request @params hash" do
+      @req.params.should respond_to :keys
+    end
+
     it "sets the @response hash", :pending => true do
       @req.commit.should respond_to :keys
     end
